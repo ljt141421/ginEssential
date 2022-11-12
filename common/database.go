85f9a86c0a33973849e2216de9820c713a684c28
liujuntao <ljt141421@aliyun.com>
2022-11-12 16:31:16 +0800

    使用viper来管理配置文件

--- common/database.go
+++ common/database.go
@@ -1,7 +1,9 @@
 package common
 
 import (
+	"fmt"
 	"ginEssential/model"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,7 +12,14 @@ var DB *gorm.DB
 
 func initDB() *gorm.DB {
 
-	dsn := "root:root@tcp(101.43.169.25:3306)/personal?charset=utf8mb4&parseTime=True&loc=Local"
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	dsn := fmt.Sprintf("%s:%st@tcp(%s:%s)/%sl?charset=utf8mb4&parseTime=True&loc=Local",
+		username, password, host, port, database)
+	//dsn := "root:root@tcp(101.43.169.25:3306)/personal?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
 
+	initConfig()
 	r := gin.Default()
 	r = CollectRouter(r)
 	panic(r.Run()) // listen and serve on 0.0.0.0:8080
 
+}
+
+func initConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("")
+	}
 }
